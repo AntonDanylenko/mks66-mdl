55ed9468379c2506ecf7e7e7443fd08141800c91
AntonDanylenko <adanylenko@stuy.edu>
2019-05-15 15:38:19 -0400

    s

--- matrix.py
+++ matrix.py
@@ -89,7 +89,7 @@ def scalar_mult( matrix, s ):
     for r in range( len( matrix[0] ) ):
         for c in range( len(matrix) ):
             matrix[c][r]*= s
-            
+
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
 
@@ -97,12 +97,17 @@ def matrix_mult( m1, m2 ):
     for row in m2:
         #get a copy of the next point
         tmp = row[:]
-        
+        # print(tmp)
+        # print(tmp[2])
         for r in range(4):
-            m2[point][r] = (m1[0][r] * tmp[0] +
-                            m1[1][r] * tmp[1] +
-                            m1[2][r] * tmp[2] +
-                            m1[3][r] * tmp[3])
+            m2[point][r] = (m1[0][r] *
+                            tmp[0] +
+                            m1[1][r] *
+                            tmp[1] +
+                            m1[2][r] *
+                            tmp[2] +
+                            m1[3][r] *
+                            tmp[3])
         point+= 1
 
 def new_matrix(rows = 4, cols = 4):
--- script.py
+++ script.py
@@ -46,27 +46,62 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print symbols
+    #print symbols
     for command in commands:
         if command['op']=='push':
-            stack.append([x[:] for x in tmp])
+            stack.append([x[:] for x in stack[-1]])
         elif command['op']=='pop':
             stack.pop()
         elif command['op']=='move':
-            t = make_translate(float(command[args][0]), float(command[args][1]), float(command[args][2]))
+            #print(command['args'][0])
+            t = make_translate(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
+            #print(t)
+            #print(stack[-1])
             matrix_mult( stack[-1], t )
             stack[-1] = [ x[:] for x in t]
         elif command['op']=='rotate':
-            theta = float(command[args][1]) * (math.pi / 180)
-            if command[args][0] == 'x':
+            theta = float(command['args'][1]) * (math.pi / 180)
+            if command['args'][0] == 'x':
                 t = make_rotX(theta)
-            elif command[args][0] == 'y':
+            elif command['args'][0] == 'y':
                 t = make_rotY(theta)
             else:
                 t = make_rotZ(theta)
             matrix_mult( stack[-1], t )
             stack[-1] = [ x[:] for x in t]
         elif command['op']=='scale':
-            t = make_scale(float(command[args][0]), float(command[args][1]), float(command[args][2]))
+            t = make_scale(float(command['args'][0]), float(command['args'][1]), float(command['args'][2]))
             matrix_mult( stack[-1], t )
             stack[-1] = [ x[:] for x in t]
+        elif command['op']=='box':
+            add_box(tmp,
+                    float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                    float(command['args'][3]), float(command['args'][4]), float(command['args'][5]))
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+        elif command['op']=='sphere':
+            add_sphere(tmp,
+                       float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                       float(command['args'][3]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+        elif command['op']=='torus':
+            add_torus(tmp,
+                      float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                      float(command['args'][3]), float(command['args'][4]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+        elif command['op']=='line':
+            add_edge( tmp,
+                      float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                      float(command['args'][3]), float(command['args'][4]), float(command['args'][5]) )
+            matrix_mult( stack[-1], tmp )
+            draw_lines(tmp, screen, zbuffer, color)
+            tmp = []
+        elif command['op']=='save':
+            save_extension(screen, command['args'][0])
+        elif command['op']=='display':
+            display(screen)
